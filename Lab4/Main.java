ded961ec56f86ce1ec03a78b5f32660a25dbb393
DaemonNikolay <nikolay-eckert@mail.ru>
2017-09-15 02:00:18 +0700

    Лаба №4 (работа с файлами)

---
+++ Lab4/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        Scanner file;
+
+        try
+        {
+            file = new Scanner(new File("C:\\Users\\User\\IdeaProjects\\lab4\\src\\com\\company\\file.txt"));
+
+            int countWord = 0;
+            int countLetter = 0;
+            int countLine = 0;
+
+            Map<Character, Integer> dictionaryLetter = new HashMap<>();
+
+            while (file.hasNext())
+            {
+                countLine++;
+                countWord++;
+                String word = file.next();
+                for (int i = 0; i < word.length(); i++)
+                {
+                    if (dictionaryLetter.containsKey(word.charAt(i)))
+                    {
+                        int tempFrequency = dictionaryLetter.get(word.charAt(i)) + 1;
+                        dictionaryLetter.remove(word.charAt(i));
+                        dictionaryLetter.put(word.charAt(i), tempFrequency);
+                    }
+                    else
+                    {
+                        dictionaryLetter.put(word.charAt(i), 1);
+                    }
+                }
+                countLetter += word.length();
+            }
+            file.close();
+
+            System.out.println("Количество слов: " + countWord);
+            System.out.println("Количество букв: " + countLetter);
+            System.out.println("Количество строк: " + countLine);
+            System.out.println("Частотный анализ: " + dictionaryLetter);
+        }
+        catch (Exception e)
+        {
+            System.out.println("Ошибка!");
+        }
+    }
+}
