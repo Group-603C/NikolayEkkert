75c8267d16a1cf7e05ca395778f882b048503a30
DaemonNikolay <nikolay-eckert@mail.ru>
2017-09-24 01:53:58 +0700

    Проверка на крайние случаи массива учтена

--- Lab2/lab2/src/com/company/Main.java
+++ Lab2/lab2/src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main
@@ -8,71 +9,143 @@ public class Main
 
     public static int GetMin(int[] arrayForFindingTheMinimum)
     {
-        int minimum = Integer.MAX_VALUE;
-        for (int i = 0; i < arrayForFindingTheMinimum.length; i++)
+        if (arrayForFindingTheMinimum.length != 0 || arrayForFindingTheMinimum != null)
         {
-            minimum = Math.min(minimum, arrayForFindingTheMinimum[i]);
-        }
+            int minimum = Integer.MAX_VALUE;
+            for (int i = 0; i < arrayForFindingTheMinimum.length; i++)
+            {
+                minimum = Math.min(minimum, arrayForFindingTheMinimum[i]);
+            }
 
-        return minimum;
+            return minimum;
+        }
+        else
+        {
+            return Integer.MAX_VALUE;
+        }
     }
 
     public static int GetMax(int[] arrayForFindingTheMaximum)
     {
-        int maximum = Integer.MIN_VALUE;
-        for (int i = 0; i < arrayForFindingTheMaximum.length; i++)
+        if (arrayForFindingTheMaximum.length != 0 && arrayForFindingTheMaximum != null)
         {
-            maximum = Math.max(maximum, arrayForFindingTheMaximum[i]);
-        }
+            int maximum = Integer.MIN_VALUE;
+            for (int i = 0; i < arrayForFindingTheMaximum.length; i++)
+            {
+                maximum = Math.max(maximum, arrayForFindingTheMaximum[i]);
+            }
 
-        return maximum;
+            return maximum;
+        }
+        else
+        {
+            return Integer.MIN_VALUE;
+        }
     }
 
     public static float ArithmeticMean(int[] arrayForFindingTheArithmeticMean)
     {
-        int valueArithmeticMean = 0;
-        for (int i = 0; i < arrayForFindingTheArithmeticMean.length; i++)
+        if (arrayForFindingTheArithmeticMean.length != 0 && arrayForFindingTheArithmeticMean != null)
         {
-            valueArithmeticMean += arrayForFindingTheArithmeticMean[i];
-        }
+            int valueArithmeticMean = 0;
+            for (int i = 0; i < arrayForFindingTheArithmeticMean.length; i++)
+            {
+                valueArithmeticMean += arrayForFindingTheArithmeticMean[i];
+            }
 
-        return valueArithmeticMean / arrayForFindingTheArithmeticMean.length;
+            return (float) valueArithmeticMean / arrayForFindingTheArithmeticMean.length;
+        }
+        else
+        {
+            return 0;
+        }
     }
 
     public static float GetMedianOfNumber(int[] arrayForFindingTheMedianOfNumber)
     {
-        Arrays.sort(arrayForFindingTheMedianOfNumber);
-        if (arrayForFindingTheMedianOfNumber.length % 2 == 0)
+        if (arrayForFindingTheMedianOfNumber.length != 0 && arrayForFindingTheMedianOfNumber != null)
         {
-            return ((arrayForFindingTheMedianOfNumber[arrayForFindingTheMedianOfNumber.length / 2] + arrayForFindingTheMedianOfNumber[arrayForFindingTheMedianOfNumber.length / 2 - 1]) / 2f);
-        }
+            Arrays.sort(arrayForFindingTheMedianOfNumber);
+            if (arrayForFindingTheMedianOfNumber.length % 2 == 0)
+            {
+                return ((arrayForFindingTheMedianOfNumber[arrayForFindingTheMedianOfNumber.length / 2] + arrayForFindingTheMedianOfNumber[arrayForFindingTheMedianOfNumber.length / 2 - 1]) / 2f);
+            }
 
-        return arrayForFindingTheMedianOfNumber[arrayForFindingTheMedianOfNumber.length / 2];
+            return arrayForFindingTheMedianOfNumber[arrayForFindingTheMedianOfNumber.length / 2];
+        }
+        else
+        {
+            return 0;
+        }
     }
 
     public static double GetGeometricMean(int[] arrayForFindingTheGeometricMean)
     {
-        double multiplicationOfNumber = 1f;
-        for (int i = 0; i < arrayForFindingTheGeometricMean.length; i++)
+        if (arrayForFindingTheGeometricMean.length != 0 || arrayForFindingTheGeometricMean != null)
         {
-            multiplicationOfNumber *= (double) arrayForFindingTheGeometricMean[i];
+            if (arrayForFindingTheGeometricMean.equals(0))
+            {
+                return 0;
+            }
+            else
+            {
+                double multiplicationOfNumber = 1f;
+                for (int i = 0; i < arrayForFindingTheGeometricMean.length; i++)
+                {
+                    multiplicationOfNumber *= (double) arrayForFindingTheGeometricMean[i];
+                }
+
+                return (Math.pow(multiplicationOfNumber, 1d / arrayForFindingTheGeometricMean.length));
+            }
         }
-        return (Math.pow(multiplicationOfNumber, 1d / arrayForFindingTheGeometricMean.length));
+        else
+        {
+            return 0;
+        }
+    }
+
+    public static int[] InputArray()
+    {
+        while (true)
+        {
+            try
+            {
+                System.out.print("Введите количество цифр: ");
+                Scanner input = new Scanner(System.in);
+                int lengthArray = input.nextInt();
+//                int lengthArray = 8;
+
+                int[] arrayNumber = new int[lengthArray];
+
+                for (int i = 0; i < lengthArray; i++)
+                {
+                    System.out.print(i + "Введите значение: ");
+                    arrayNumber[i] = input.nextInt();
+                }
+
+
+//                arrayNumber[0] = 10;
+//                arrayNumber[1] = 1;
+//                arrayNumber[2] = 2;
+//                arrayNumber[3] = 3;
+//                arrayNumber[4] = 4;
+//                arrayNumber[5] = 5;
+//                arrayNumber[6] = 6;
+//                arrayNumber[7] = 7;
+
+                return arrayNumber;
+            }
+            catch (NegativeArraySizeException e)
+            {
+                System.out.println("\tОшибка, длина массива отрицательна!");
+            }
+        }
+
     }
 
     public static void main(String[] args)
     {
-        System.out.print("Введите количество цифр: ");
-        Scanner input = new Scanner(System.in);
-        int lengthArrayNumber = input.nextInt();
-
-        int[] arrayNumber = new int[lengthArrayNumber];
-
-        for (int i = 0; i < lengthArrayNumber; i++)
-        {
-            System.out.print("Введите значение: ");
-            arrayNumber[i] = input.nextInt();
-        }
+        int[] arrayNumber = InputArray();
 
         System.out.println("\nМинимум: " + GetMin(arrayNumber));
         System.out.println("Максимум: " + GetMax(arrayNumber));
