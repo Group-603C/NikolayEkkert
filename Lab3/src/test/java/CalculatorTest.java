2810d7547cbf744c93ec754e6832874e5ebc1cfc
DaemonNikolay <nikolay-eckert@mail.ru>
2017-10-10 00:52:36 +0700

    Очистка от лишнего

--- Lab3/src/test/java/CalculatorTest.java
+++ Lab3/src/test/java/CalculatorTest.java
@@ -6,24 +6,8 @@ public class CalculatorTest
     public void calculate() throws Exception
     {
         Calculator exemplar1 = new Calculator();
-        double expect = 74.2;
-        double actual = exemplar1.calculate("6.2+(7+10)*4");
+        double expect = 21d;
+        double actual = exemplar1.calculate("4*5-2+3");
         assertEquals("One test", expect, actual, 0.0001);
-
-//        expect = 74.2;
-//        actual = exemplar1.calculate("6.2+7+10*4");
-//        assertEquals("Two test", expect, actual, 0.0001);
-//
-//        expect = 74.2;
-//        actual = exemplar1.calculate("6.2+7+10*4");
-//        assertEquals("Three test", expect, actual, 0.0001);
-//
-//        expect = 74.2;
-//        actual = exemplar1.calculate("2+3-4*5");
-//        assertEquals("Four test", expect, actual, 0.0001);
-//
-//        expect = 74.2;
-//        actual = exemplar1.calculate("4*5-2+3");
-//        assertEquals("Five test", expect, actual, 0.0001);
     }
 }
