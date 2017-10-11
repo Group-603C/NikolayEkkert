079e5fc0476139748f825383bd795dcd14dae56e
DaemonNikolay <nikolay-eckert@mail.ru>
2017-10-11 18:13:38 +0700

    Механизм через StringBuilder, корректный поиск меток

--- Lab5/src/main/java/Formatter.java
+++ Lab5/src/main/java/Formatter.java
@@ -9,4 +9,36 @@ public class Formatter
 
         return formatString;
     }
+
+    public String build1(String formatString, Object... arguments)
+    {
+        StringBuilder temp = new StringBuilder(formatString);
+        StringBuilder numberLabel = new StringBuilder();
+
+        char[] arrayFormatString = formatString.toCharArray();
+        boolean flag = false;
+
+        for (char element : arrayFormatString)
+        {
+            if (element == '{')
+            {
+//                System.out.println("2");
+                flag = true;
+            }
+            else if (element == '}')
+            {
+                System.out.println("3");
+                flag = false;
+                numberLabel.delete(0, numberLabel.length());
+            }
+            else if (flag)
+            {
+//                System.out.println("1");
+                numberLabel.append(element);
+//                System.out.println(numberLabel);
+            }
+        }
+
+        return temp.toString();
+    }
 }
