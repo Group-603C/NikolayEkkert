f30676d15dff7e4602553e1d3b5d91dbd0ee93d5
DaemonNikolay <nikolay-eckert@mail.ru>
2017-10-11 20:54:52 +0700

    Вариант замены меток на аргументы

--- Lab5/src/main/java/Formatter.java
+++ Lab5/src/main/java/Formatter.java
@@ -1,3 +1,5 @@
+import com.sun.deploy.util.StringUtils;
+
 public class Formatter
 {
     public String build(String formatString, Object... arguments)
@@ -12,33 +14,55 @@ public class Formatter
 
     public String build1(String formatString, Object... arguments)
     {
-        StringBuilder temp = new StringBuilder(formatString);
+        StringBuilder editedFormatString = new StringBuilder(formatString);
         StringBuilder numberLabel = new StringBuilder();
 
         char[] arrayFormatString = formatString.toCharArray();
         boolean flag = false;
+        int countOpenQuote = 0;
+        int countCloseQuote = 0;
+
+        int tempOpen = 0;
+        int tempClose = 0;
 
         for (char element : arrayFormatString)
         {
+            countOpenQuote++;
+            countCloseQuote++;
             if (element == '{')
             {
+                tempOpen = countOpenQuote;
+//                System.out.println("Open: " + countOpenQuote + "\t" + element);
 //                System.out.println("2");
                 flag = true;
             }
             else if (element == '}')
             {
-                System.out.println("3");
+                tempClose = countCloseQuote;
+//                System.out.println("Close: " + countCloseQuote + "\t" + element);
+//                System.out.println("3");
                 flag = false;
+//                System.out.println(numberLabel);
+
+                String temp = numberLabel.toString();
+                int numberArgument = Integer.parseInt(temp);
+//                editedFormatString.replace(countOpenQuote, countCloseQuote, arguments[numberArgument].toString());
+
+                System.out.println(tempOpen + "\t" + tempClose);
+                editedFormatString.replace(tempOpen-1, tempClose, "!!");
+//                editedFormatString.replace(tempOpen-1, tempClose, arguments[numberArgument].toString());
+
                 numberLabel.delete(0, numberLabel.length());
             }
             else if (flag)
             {
 //                System.out.println("1");
                 numberLabel.append(element);
+//                System.out.println(i);
 //                System.out.println(numberLabel);
             }
         }
 
-        return temp.toString();
+        return editedFormatString.toString();
     }
 }
