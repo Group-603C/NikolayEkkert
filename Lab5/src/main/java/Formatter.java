c5715e898e1f5b83a7ec2b5ad4c641f72263ee09
DaemonNikolay <nikolay-eckert@mail.ru>
2017-10-14 20:44:35 +0700

    Чистка от мусора

--- Lab5/src/main/java/Formatter.java
+++ Lab5/src/main/java/Formatter.java
@@ -1,38 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-
-class Container {
-    private int IndexOpenQuote;
-    private int IndexCloseQuote;
-    private String IndexArgument;
-
-    public void setIndexArgument(String text) {
-        this.IndexArgument = text;
-    }
-
-    public void setIndexOpenQuote(int index) {
-        this.IndexOpenQuote = index;
-    }
-
-    public void setIndexCloseQuote(int index) {
-        this.IndexCloseQuote = index;
-    }
-
-    public String getIndexArgument( ) {
-        return this.IndexArgument;
-    }
-
-    public int getIndexOpenQuote( ) {
-        return this.IndexOpenQuote;
-    }
-
-    public int getIndexCloseQuote( ) {
-        return this.IndexCloseQuote;
-    }
-}
-
 public class Formatter {
-
 
     public String build(String formatString, Object... arguments) {
 
@@ -56,12 +22,6 @@ public class Formatter {
 
         int tempOpen = 0;
         int tempClose = 0;
-
-        //        List<Integer> indexOpenQuote = new ArrayList<Integer>( );
-        //        List<Integer> indexCloseQuote = new ArrayList<Integer>( );
-        //        List<Integer> indexArgument = new ArrayList<Integer>( );
-
-//        int[] array = new int[arguments.length];
         int counter = 0;
 
         Container[] array = new Container[0];
@@ -71,12 +31,11 @@ public class Formatter {
         catch (NullPointerException e) {
 
         }
-//        Container[] exemplar = new Container[arguments.length];
+
         for (char element : arrayFormatString) {
 
             countOpenQuote++;
             countCloseQuote++;
-
 
             if (element == '{') {
                 tempOpen = countOpenQuote;
@@ -98,23 +57,8 @@ public class Formatter {
                         exemplar.setIndexCloseQuote(tempClose);
                         exemplar.setIndexArgument(arguments[numberArgument].toString( ));
 
-
-//                        System.out.println(counter);
                         array[counter] = exemplar;
-//                        System.out.println(array[counter].getIndexOpenQuote());
                         counter++;
-//                        System.out.println("rrr\t" + array[counter].getIndexOpenQuote());
-//                        System.out.println("fff\t" + array[counter]);
-
-//                        array[i] = exemplar.setIndexOpenQuote(tempOpen);
-//                        exemplar[counter].setIndexCloseQuote(tempClose);
-//                        exemplar[counter].setIndexOpenQuote(tempOpen);
-//                        exemplar[counter].setIndexArgument(arguments[numberArgument].toString( ));
-//                        System.out.println(exemplar[counter].getIndexOpenQuote( ));
-
-                        //                        indexOpenQuote.add(tempOpen);
-                        //                        indexCloseQuote.add(tempClose);
-                        //                        indexArgument.add(numberArgument);
 
                     }
 
@@ -135,16 +79,11 @@ public class Formatter {
             }
         }
 
-//        System.out.println("fdfdf\t" + array[0]);
-
         try {
 
-
             for (int i = arguments.length - 1; i >= 0; i--) {
-//                System.out.println("1");
-//                System.out.println(array[i].getIndexOpenQuote( ) + "\t" + array[i].getIndexCloseQuote( ) + "\t" + array[i].getIndexArgument( ));
+
                 try {
-//editedFormatString.replace(indexOpenQuote.get(i) - 1, indexCloseQuote.get(i), arguments[indexArgument.get(i)].toString( ));
                     editedFormatString.replace(array[i].getIndexOpenQuote( ) - 1, array[i].getIndexCloseQuote( ), array[i].getIndexArgument( ));
                 }
                 catch (Exception e) {
@@ -156,7 +95,37 @@ public class Formatter {
 
         }
 
-//        System.out.println(editedFormatString);
         return editedFormatString.toString( );
+    }
+
+
+    class Container {
+        private int IndexOpenQuote;
+        private int IndexCloseQuote;
+        private String IndexArgument;
+
+        public void setIndexArgument(String text) {
+            this.IndexArgument = text;
+        }
+
+        public void setIndexOpenQuote(int index) {
+            this.IndexOpenQuote = index;
+        }
+
+        public void setIndexCloseQuote(int index) {
+            this.IndexCloseQuote = index;
+        }
+
+        public String getIndexArgument( ) {
+            return this.IndexArgument;
+        }
+
+        public int getIndexOpenQuote( ) {
+            return this.IndexOpenQuote;
+        }
+
+        public int getIndexCloseQuote( ) {
+            return this.IndexCloseQuote;
+        }
     }
 }
--- Lab5/src/main/java/Main.java
+++ Lab5/src/main/java/Main.java
@@ -6,47 +6,5 @@ public class Main {
         String text = exemplar.build("здесь {0}, а здесь {1}, а вот {2}", "мама", "папа");
 
         System.out.println(text);
-
-//        Container[] array = new Container[3];
-//        for (int i = 0; i < 3; i++) {
-//            Container exem = new Container( );
-//            exem.setIndexArgument(Integer.toString(-10));
-//            exem.setIndexOpenQuote(i + 15);
-//            exem.setIndexCloseQuote(i - 170);
-//            array[i] = exem;
-//        }
-//
-//        for (int i = 0; i < 3; i++) {
-//            System.out.println(array[i].getIndexOpenQuote( ) + "\t" + array[i].getIndexArgument( ) + "\t" + array[i].getIndexCloseQuote( ));
-//        }
-//
-//        Container[] array = new Container[3];
-//
-//        Container exem = new Container( );
-//        exem.setIndexOpenQuote(111);
-//        exem.setIndexCloseQuote(1222);
-//        exem.setIndexArgument("ZZZZ");
-//        array[0] = exem;
-//
-//        exem.setIndexOpenQuote(2222);
-//        exem.setIndexCloseQuote(2222);
-//        exem.setIndexArgument("VVVV");
-//        array[1] = exem;
-//
-//        exem.setIndexOpenQuote(6666);
-//        exem.setIndexCloseQuote(224343422);
-//        exem.setIndexArgument("FFFF");
-//        array[2] = exem;
-//
-//        System.out.println(array[0]);
-//        System.out.println(array[1]);
-//        System.out.println(array[2]);
-
-
-//        exem[0].setIndexArgument("111");
-//        exem[1].setIndexCloseQuote(1);
-//        exem[1].setIndexArgument("222");
-//        exem[2].setIndexArgument("333");
-//        System.out.println(exem[1].getIndexArgument());
     }
 }
