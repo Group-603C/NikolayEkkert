4d7a55f96354c43ab83afe538a18de9d3bb0afb8
DaemonNikolay <nikolay-eckert@mail.ru>
2017-10-17 20:51:25 +0700

    Перевод тестов на английский язык

--- Lab5/src/test/java/FormatterTest.java
+++ Lab5/src/test/java/FormatterTest.java
@@ -26,32 +26,32 @@ public class FormatterTest {
 
         Formatter exemplar = new Formatter( );
 
-        String value = exemplar.build("здесь {0}, а здесь {1}, а вот {2}", "мама", "папа", "Я");
-        String expected = "здесь мама, а здесь папа, а вот Я";
+        String value = exemplar.build("here {0} here {1}, but {2}", "mom", "dad", "I");
+        String expected = "here mom here dad, but I";
         assertEquals("Correct writing and sending", expected, value);
 
-        value = exemplar.build("Вот и наступил {3} год, пора мне пройти {1} страны, посетить {0} музея и пролететь {2} км", 32, 196, 22, 777);
-        expected = "Вот и наступил 777 год, пора мне пройти 196 страны, посетить 32 музея и пролететь 22 км";
+        value = exemplar.build("Here come {3}, it's time for me to go through {1} of the country, visit {0} Museum and {2} km", 32, 196, 22, 777);
+        expected = "Here come 777, it's time for me to go through 196 of the country, visit 32 Museum and 22 km";
         assertEquals("Not consistent arrangement of labels in the template", expected, value);
 
-        value = exemplar.build("Нельзя {2} так взять и {3} {0} по {1}", "лабы", "Java", "так", "сдать", "иначе кирдык");
-        expected = "Нельзя так так взять и сдать лабы по Java";
+        value = exemplar.build("Not {2} to take and {3} {0} {1}", "the lab", "Java", "so", "pass", "otherwise");
+        expected = "Not so to take and pass the lab Java";
         assertEquals("Parameters more than labels", expected, value);
 
-        value = exemplar.build("здесь {0}, а здесь {1}, а вот {2}", "мама", "папа");
-        expected = "здесь мама, а здесь папа, а вот {2}";
+        value = exemplar.build("here {0} here {1}, but {2}", "mom", "dad");
+        expected = "here mom here dad, but {2}";
         assertEquals("Parameters less than labels", expected, value);
 
-        value = exemplar.build("здесь {0, а здесь {1}, а вот {2}", "мама", "папа");
-        expected = "здесь {0, а здесь {1}, а вот {2}";
+        value = exemplar.build("here {0, here {1}, but {2}", "mom", "dad");
+        expected = "here {0, here {1}, but {2}";
         assertEquals("Not a closed label", expected, value);
 
-        value = exemplar.build("", "мама", "папа");
+        value = exemplar.build("", "mom", "dad");
         expected = "";
         assertEquals("Blank template without labels", expected, value);
 
-        value = exemplar.build("здесь 0, а здесь {1}, а вот {2}", "мама", "папа");
-        expected = "здесь 0, а здесь папа, а вот {2}";
+        value = exemplar.build("here 0, here {1}, but {2}", "mom", "dad");
+        expected = "here 0, here dad, but {2}";
         assertEquals("Instead of the 0 label, the digit 0", expected, value);
 
         value = exemplar.build("", "");
@@ -62,48 +62,48 @@ public class FormatterTest {
         expected = "";
         assertEquals("The pattern and the argument is null", expected, value);
 
-        value = exemplar.build("Здесь int значение {0}", 20543);
-        expected = "Здесь int значение 20543";
+        value = exemplar.build("Here int value {0}", 20543);
+        expected = "Here int value 20543";
         assertEquals("Parameter of type int", expected, value);
 
-        value = exemplar.build("Здесь float значение {0}", 0.68f);
-        expected = "Здесь float значение 0.68";
+        value = exemplar.build("Here the float value is {0}", 0.68f);
+        expected = "Here the float value is 0.68";
         assertEquals("Parameter of type float", expected, value);
 
-        value = exemplar.build("Здесь double значение {0}", 3.14d);
-        expected = "Здесь double значение 3.14";
+        value = exemplar.build("Here the double value {0}", 3.14d);
+        expected = "Here the double value 3.14";
         assertEquals("Parameter of type double", expected, value);
 
-        value = exemplar.build("Здесь char значение {0}", 'V');
-        expected = "Здесь char значение V";
+        value = exemplar.build("Here the char value {0}", 'V');
+        expected = "Here the char value V";
         assertEquals("Parameter of type char", expected, value);
 
         Object objZero = "Object";
-        value = exemplar.build("Здесь object значение {0}", objZero);
-        expected = "Здесь object значение Object";
+        value = exemplar.build("Here object the value {0}", objZero);
+        expected = "Here object the value Object";
         assertEquals("Parameter of type Object with the number", expected, value);
 
         Object objOne = 1234.54;
-        value = exemplar.build("Здесь object значение {0}", objOne);
-        expected = "Здесь object значение 1234.54";
+        value = exemplar.build("Here object the value {0}", objOne);
+        expected = "Here object the value 1234.54";
         assertEquals("Parameter of type Object with the line", expected, value);
 
-        value = exemplar.build("Здесь boolean значение {0}", true);
-        expected = "Здесь boolean значение true";
+        value = exemplar.build("Here boolean value {0}", true);
+        expected = "Here boolean value true";
         assertEquals("Parameter of type boolean", expected, value);
 
         String text = "text";
-        value = exemplar.build("Здесь String значение {0}", text);
-        expected = "Здесь String значение text";
+        value = exemplar.build("Here the String value {0}", text);
+        expected = "Here the String value text";
         assertEquals("Parameter of type String", expected, value);
 
-        value = exemplar.build("Без параметров");
-        expected = "Без параметров";
+        value = exemplar.build("Without parameters");
+        expected = "Without parameters";
         assertEquals("Absence of the parameter", expected, value);
 
         Box box = new Box(111,222,333);
-        value = exemplar.build("Параметр класс: {0}", box);
-        expected = "Параметр класс: Размер 111 * 222 * 333";
+        value = exemplar.build("Parameter class: {0}", box);
+        expected = "Parameter class: Размер 111 * 222 * 333";
         assertEquals("The transfer of a class with three fields and an overload of toString", expected, value);
     }
 }
