f2a38c100cee673555ffe27385997e14314bfc2f
DaemonNikolay <nikolay-eckert@mail.ru>
2017-10-17 20:32:23 +0700

    Вместо номера теста вставлено описание теста

--- Lab5/src/test/java/FormatterTest.java
+++ Lab5/src/test/java/FormatterTest.java
@@ -28,23 +28,23 @@ public class FormatterTest {
 
         String value = exemplar.build("здесь {0}, а здесь {1}, а вот {2}", "мама", "папа", "Я");
         String expected = "здесь мама, а здесь папа, а вот Я";
-        assertEquals("One test", expected, value);
+        assertEquals("Корректный ввод и передача", expected, value);
 
         value = exemplar.build("Вот и наступил {3} год, пора мне пройти {1} страны, посетить {0} музея и пролететь {2} км", 32, 196, 22, 777);
         expected = "Вот и наступил 777 год, пора мне пройти 196 страны, посетить 32 музея и пролететь 22 км";
-        assertEquals("Two test", expected, value);
+        assertEquals("Не последовательная расстановка меток в шаблоне", expected, value);
 
         value = exemplar.build("Нельзя {2} так взять и {3} {0} по {1}", "лабы", "Java", "так", "сдать", "иначе кирдык");
         expected = "Нельзя так так взять и сдать лабы по Java";
-        assertEquals("Three test", expected, value);
+        assertEquals("Параметров больше, чем меток", expected, value);
 
         value = exemplar.build("здесь {0}, а здесь {1}, а вот {2}", "мама", "папа");
         expected = "здесь мама, а здесь папа, а вот {2}";
-        assertEquals("Four test", expected, value);
+        assertEquals("Параметров меньше, чем меток", expected, value);
 
         value = exemplar.build("здесь {0, а здесь {1}, а вот {2}", "мама", "папа");
         expected = "здесь {0, а здесь {1}, а вот {2}";
-        assertEquals("Five test", expected, value);
+        assertEquals("Не закрытая метка", expected, value);
 
         value = exemplar.build("", "мама", "папа");
         expected = "";
@@ -52,58 +52,58 @@ public class FormatterTest {
 
         value = exemplar.build("здесь 0, а здесь {1}, а вот {2}", "мама", "папа");
         expected = "здесь 0, а здесь папа, а вот {2}";
-        assertEquals("Seven test", expected, value);
+        assertEquals("Вместо метки 0, цифра 0", expected, value);
 
         value = exemplar.build("", "");
         expected = "";
-        assertEquals("Eight test", expected, value);
+        assertEquals("Передача пустого шаблона", expected, value);
 
         value = exemplar.build(null, null);
         expected = "";
-        assertEquals("Nine test", expected, value);
+        assertEquals("Шаблон и аргумент null", expected, value);
 
         value = exemplar.build("Здесь int значение {0}", 20543);
         expected = "Здесь int значение 20543";
-        assertEquals("Ten test", expected, value);
+        assertEquals("Параметр типа int", expected, value);
 
         value = exemplar.build("Здесь float значение {0}", 0.68f);
         expected = "Здесь float значение 0.68";
-        assertEquals("Eleven test", expected, value);
+        assertEquals("Параметр типа float", expected, value);
 
         value = exemplar.build("Здесь double значение {0}", 3.14d);
         expected = "Здесь double значение 3.14";
-        assertEquals("Twelve test", expected, value);
+        assertEquals("Параметр типа double", expected, value);
 
         value = exemplar.build("Здесь char значение {0}", 'V');
         expected = "Здесь char значение V";
-        assertEquals("Thirteen test", expected, value);
+        assertEquals("Параметр типа char", expected, value);
 
         Object objZero = "Object";
         value = exemplar.build("Здесь object значение {0}", objZero);
         expected = "Здесь object значение Object";
-        assertEquals("Fourteen test", expected, value);
+        assertEquals("Параметр типа Object с числом", expected, value);
 
         Object objOne = 1234.54;
         value = exemplar.build("Здесь object значение {0}", objOne);
         expected = "Здесь object значение 1234.54";
-        assertEquals("Fifteen test", expected, value);
+        assertEquals("Параметр типа Object со строкой", expected, value);
 
         value = exemplar.build("Здесь boolean значение {0}", true);
         expected = "Здесь boolean значение true";
-        assertEquals("Sixteen test", expected, value);
+        assertEquals("Параметр типа boolean", expected, value);
 
         String text = "text";
         value = exemplar.build("Здесь String значение {0}", text);
         expected = "Здесь String значение text";
-        assertEquals("Seventeen test", expected, value);
+        assertEquals("Параметр типа String", expected, value);
 
         value = exemplar.build("Без параметров");
         expected = "Без параметров";
-        assertEquals("Eighteen test", expected, value);
+        assertEquals("Отсутствие параметра", expected, value);
 
         Box box = new Box(111,222,333);
-        value = exemplar.build("Без параметров: {0}", box);
-        expected = "Без параметров: Размер 111 * 222 * 333";
-        assertEquals("Nineteen test", expected, value);
+        value = exemplar.build("Параметр класс: {0}", box);
+        expected = "Параметр класс: Размер 111 * 222 * 333";
+        assertEquals("Передача класса с тремя полями и перегрузка toString", expected, value);
     }
 }
