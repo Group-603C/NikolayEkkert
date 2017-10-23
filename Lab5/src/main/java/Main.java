3c8663ba087070f21118aafb46158f03f1808ce7
DaemonNikolay <nikolay-eckert@mail.ru>
2017-10-23 22:55:57 +0700

    Main теперь с перехватом исключений

--- Lab5/src/main/java/Main.java
+++ Lab5/src/main/java/Main.java
@@ -3,8 +3,13 @@ public class Main {
     public static void main(String[] args) {
 
         Formatter exemplar = new Formatter( );
-        String text = exemplar.build("здесь {0}, а здесь {1}, а вот {2}", "мама", "папа");
+        try {
 
-        System.out.println(text);
+            String instance = exemplar.build("здесь {0}, а здесь {1}, а вот {2}", "мама", "папа");
+        }
+        catch (IndexOutOfBoundsException e) {
+        }
+        catch (NumberFormatException e) {
+        }
     }
 }
