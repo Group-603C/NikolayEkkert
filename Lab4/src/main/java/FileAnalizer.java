601b7d54301c5ca21744fe5a9ef60939b6216913
DaemonNikolay <nikolay-eckert@mail.ru>
2017-11-01 00:22:46 +0700

    Уменьшена вложенность, требуется рефакторинг

--- Lab4/src/main/java/FileAnalizer.java
+++ Lab4/src/main/java/FileAnalizer.java
@@ -5,38 +5,37 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
-public class FileAnalizer
-{
+public class FileAnalizer {
+
     private Object[] arrayLinesFile;
 
-    public FileAnalizer(String path)
-    {
-        try
-        {
-            arrayLinesFile = Files.lines(Paths.get(path), StandardCharsets.UTF_8).toArray();
+    public FileAnalizer(String path) {
+
+        try {
+
+            arrayLinesFile = Files.lines(Paths.get(path), StandardCharsets.UTF_8).toArray( );
         }
-        catch (IOException e)
-        {
+        catch (IOException e) {
+
             System.out.println("Ошибка, файл не найден!");
         }
     }
 
-    public int CountLetters()
-    {
+    public int CountLetters( ) {
         int countLetter = 0;
 
-        for (Object line : arrayLinesFile)
-        {
+        for (Object line : arrayLinesFile) {
+
             String tempLineForLetter = (String) line;
-            if (tempLineForLetter.length() != 0)
-            {
-                char[] tempSymbol = tempLineForLetter.toCharArray();
-                for (char element : tempSymbol)
-                {
-                    if (element != ' ')
-                    {
-                        countLetter++;
-                    }
+            if (tempLineForLetter.length( ) == 0) {
+                continue;
+            }
+
+            char[] tempSymbol = tempLineForLetter.toCharArray( );
+            for (char element : tempSymbol) {
+
+                if (element != ' ') {
+                    countLetter++;
                 }
             }
         }
@@ -44,15 +43,14 @@ public class FileAnalizer
         return countLetter;
     }
 
-    public int CountWords()
-    {
-        int countWord = 0;
+    public int CountWords( ) {
 
-        for (Object line : arrayLinesFile)
-        {
-            String tempLineForWord = (String) line;
-            if (tempLineForWord.length() != 0)
-            {
+        int countWord = 0;
+        for (Object line : arrayLinesFile) {
+
+            String tempLineForWord = line.toString();
+            if (tempLineForWord.length( ) != 0) {
+
                 countWord += tempLineForWord.split(" +").length;
             }
         }
@@ -60,43 +58,37 @@ public class FileAnalizer
         return countWord;
     }
 
-    public int CountLines()
-    {
-        try
-        {
+    public int CountLines( ) {
+
+        try {
+
             return arrayLinesFile.length;
         }
-        catch (NullPointerException e)
-        {
+        catch (NullPointerException e) {
+
             System.out.println("Ошибка, массив строк получен не был!");
             return 0;
         }
     }
 
-    public Map<Character, Integer> CountFrequencyCharacteristic()
-    {
-        Map<Character, Integer> countSymbol = new HashMap<Character, Integer>();
+    public Map<Character, Integer> CountFrequencyCharacteristic( ) {
+        Map<Character, Integer> countSymbol = new HashMap<Character, Integer>( );
 
-        for (Object line : arrayLinesFile)
-        {
+        for (Object line : arrayLinesFile) {
+
             String tempLineForLetter = (String) line;
-            if (tempLineForLetter.length() != 0)
-            {
-                char[] tempSymbol = tempLineForLetter.toCharArray();
-                for (char element : tempSymbol)
-                {
-                    if (element != ' ')
-                    {
-                        if (countSymbol.containsKey(element))
-                        {
-                            int tempValue = countSymbol.get(element) + 1;
-                            countSymbol.put(element, tempValue);
-                        }
-                        else
-                        {
-                            countSymbol.put(element, 1);
-                        }
+            if (tempLineForLetter.length( ) != 0) {
+
+                char[] tempSymbol = tempLineForLetter.toCharArray( );
+                for (char element : tempSymbol) {
+
+                    if (element != ' ') {
+                        
+                        countSymbol.put(element, 1);
+                        continue;
                     }
+                    int tempValue = countSymbol.get(element) + 1;
+                    countSymbol.put(element, tempValue);
                 }
             }
         }
