2ae8b914cf800e4ec5c09a348833246424e0fa10
DaemonNikolay <nikolay-eckert@mail.ru>
2017-11-19 01:25:37 +0700

    Заглушки класса Point

--- Lab6/src/com/company/Point.java
+++ Lab6/src/com/company/Point.java
@@ -1,3 +1,56 @@
 package com.company;
 
-public class Point {}
+public class Point {
+
+    Point(double x,
+          double y) {}
+
+    Point(double x,
+          double y,
+          double z) {}
+
+    double getX() {
+
+        return -1;
+    }
+
+    double getY() {
+
+        return -1;
+    }
+
+    double getZ() {
+
+        return -1;
+    }
+
+    double distanceTo(Point point) {
+
+        return -1;
+    }
+
+    PolarPoint toPolar() {
+
+        return new PolarPoint();
+    }
+
+    CylindricalPoint toCylindrical() {
+
+        return new CylindricalPoint();
+    }
+
+    static public boolean isOneStraightLine(Point first,
+                                            Point send,
+                                            Point third) {
+
+        return false;
+    }
+
+    static public boolean isOnePlane(Point first,
+                                     Point send,
+                                     Point third,
+                                     Point fourth) {
+
+        return false;
+    }
+}
