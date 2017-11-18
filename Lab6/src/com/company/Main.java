cfc2ba20235e10cf3f4eba3da6bd421810a72859
DaemonNikolay <nikolay-eckert@mail.ru>
2017-11-19 02:09:41 +0700

    Заложен фундамент тестов, в Main поюзаны классы

--- Lab6/src/com/company/Main.java
+++ Lab6/src/com/company/Main.java
@@ -3,6 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Point instance = new Point(10, 2, 54);
+        System.out.println(instance.getX());
+        System.out.println(instance.getY());
+        System.out.println(instance.getZ());
+
+        Point instnaceForComparison = new Point(12, 144, 90);
+        System.out.println(instance.distanceTo(instnaceForComparison));
+
+        PolarPoint instancePolar = instance.toPolar();
+        System.out.println(instancePolar.getPhi());
+        System.out.println(instancePolar.getRho());
+        System.out.println(instancePolar.toCartesian() instanceof Point);
+
+        CylindricalPoint instanceCylindrical = instance.toCylindrical();
+        System.out.println(instanceCylindrical.getZ());
+        System.out.println(instanceCylindrical.getPhi());
+        System.out.println(instanceCylindrical.getRho());
+        System.out.println(instanceCylindrical.toCartesian() instanceof Point);
     }
 }
---
+++ Lab6/test/com/company/PointTest.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+    @Test
+    public void getX() throws Exception {
+    }
+
+    @Test
+    public void getY() throws Exception {
+    }
+
+    @Test
+    public void getZ() throws Exception {
+    }
+
+    @Test
+    public void distanceTo() throws Exception {
+    }
+
+    @Test
+    public void toPolar() throws Exception {
+    }
+
+    @Test
+    public void toCylindrical() throws Exception {
+    }
+
+    @Test
+    public void isOneStraightLine() throws Exception {
+    }
+
+    @Test
+    public void isOnePlane() throws Exception {
+    }
+
+}
