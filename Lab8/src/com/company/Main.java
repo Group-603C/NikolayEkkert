2a12f5c898baab07e51410a047349d59b981d86e
DaemonNikolay <nikolay-eckert@mail.ru>
2017-11-19 08:17:06 +0700

    Класс Main почищен

--- Lab8/src/com/company/Main.java
+++ Lab8/src/com/company/Main.java
@@ -4,14 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Locker instance = new Locker();
-        try {
-            instance.set(12);
-        }
-        catch (LockerException e) {
-
-        }
-
-        System.out.println(instance.get());
     }
 }
