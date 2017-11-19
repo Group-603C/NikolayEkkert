fcba464ab5d1e062971b8f48252e031b7202b888
DaemonNikolay <nikolay-eckert@mail.ru>
2017-11-19 07:12:33 +0700

    Заложен фундамент с заглушками на 8-ую лабу

---
+++ Lab8/src/com/company/Locker.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Locker {
+
+    private boolean isLock;
+    private int value;
+
+
+    void set(int value){
+        
+        this.value = value;
+    }
+
+    int get(){
+
+        return value;
+    }
+
+    void lock(){
+
+    }
+
+    void unlock(){
+
+    }
+}
