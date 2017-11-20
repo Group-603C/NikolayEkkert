915fd900fbff7f5d46e713beab6e14d9c1dfac65
DaemonNikolay <nikolay-eckert@mail.ru>
2017-11-20 16:51:42 +0700

    Очистка Main

--- Lab7/src/com/company/Main.java
+++ Lab7/src/com/company/Main.java
@@ -3,16 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-        char[] temp = Integer.toBinaryString(43)
-                             .toCharArray();
-
-        StringBuilder result = new StringBuilder();
-        for (char element : temp) {
-            result.append(element == '0' ? "1" : "0");
-        }
-
-        System.out.println(Integer.parseInt(result.toString(), 2));
-
+        
     }
 }
