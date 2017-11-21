ca07e0d35ec17613942376477600bad2d508caa2
DaemonNikolay <nikolay-eckert@mail.ru>
2017-11-21 13:24:32 +0700

    Реализация распарсивания входного выражения, не закончено

--- Lab7/src/com/company/Expression.java
+++ Lab7/src/com/company/Expression.java
@@ -1,3 +1,21 @@
 package com.company;
 
-public class Expression {}
+public class Expression {
+
+    private String value;
+
+
+    Expression(String expression) {
+
+        this.value = expression;
+    }
+
+    public double analyzeExpression(){
+
+        for(char element : value.toCharArray()){
+            if(element==)
+        }
+
+        return -1d;
+    }
+}
