f6c97d7dacb5edfc89d3d6c9707517f1376fe735
DaemonNikolay <nikolay-eckert@mail.ru>
2017-11-24 19:44:04 +0700

    в Main написана проверка, удалить позже, нужно для самоудоволетворения

--- Lab8/src/com/company/Main.java
+++ Lab8/src/com/company/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Locker locker = new Locker(1);
+
+        locker.get();
+
+        locker.unlock();
+        try {
+            locker.set(3);
+        }
+        catch (LockerException e) {
+            System.out.println(Integer.MIN_VALUE);
+        }
+
+
     }
 }
