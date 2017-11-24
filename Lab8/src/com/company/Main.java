206f2298942c28a7a0d6ec5e938cbe19a790d6f6
DaemonNikolay <nikolay-eckert@mail.ru>
2017-11-25 00:43:00 +0700

    Чистка Main

--- Lab8/src/com/company/Main.java
+++ Lab8/src/com/company/Main.java
@@ -4,18 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Locker locker = new Locker(1);
-
-        locker.get();
-
-        locker.unlock();
-        try {
-            locker.set(3);
-        }
-        catch (LockerException e) {
-            System.out.println(Integer.MIN_VALUE);
-        }
-
-
     }
 }
